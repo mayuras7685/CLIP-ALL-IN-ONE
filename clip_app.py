99a86e27eb74edf2c72401d2ba51c50c115da73e
mayuras7685 <mayurasodara@gmail.com>
2024-02-08 12:46:00 +0530

    Zero Shot Text Classification Added

--- clip_app.py
+++ clip_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from backend.app_pages import image_classification
+from backend.app_pages import image_classification, text_classification
 from backend.clip_functions import download_clip_model
 from backend.load import load_yaml
 
@@ -13,7 +13,8 @@ clip_model = config['CLIP_MODEL'][0]
 model, processor, tokeniser = download_clip_model(clip_model) # load the model, processor and tokeniser - this is cached
 
 # Define menu items for each sub-app
-navigation_buttons = {"Zero Shot Image Classification": image_classification
+navigation_buttons = {"Zero Shot Image Classification": image_classification,
+                      "Zero Shot Text Classification": text_classification,
                       }
 
 st.sidebar.title('CLIP Demo')
@@ -21,3 +22,6 @@ selection = st.sidebar.radio("Go to", list(navigation_buttons.keys()))
 page = navigation_buttons[selection]
 if selection == 'Zero Shot Image Classification':
     page.write(processor, model, tokeniser)
+
+elif selection == 'Zero Shot Text Classification':
+    page.write(tokeniser, model)
