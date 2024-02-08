191a5b60070982bdb68aeb81413b569808fbd698
mayuras7685 <mayurasodara@gmail.com>
2024-02-08 12:45:22 +0530

    load_pickle

--- backend/show.py
+++ backend/show.py
@@ -23,6 +23,41 @@ def plot_results(df, y_label, x_label, color_discrete_sequence):
     return fig
 
 
+def plot_caption_clusters(df, x, y, hover_data, color, size, text, opacity=0.7):
+    fig = px.scatter(df, x=x, y=y, hover_data=hover_data, color=color, opacity=opacity, size=size, text=text)
+
+    fig.update_layout(
+        margin=dict(l=40, r=40, t=40, b=40),  # set the margins of the plot
+        height=600,
+        width=1050,
+        title='Clustering Captions',  # set the title of the plot
+
+        font=dict(family='Arial', size=12),  # set the font family and size
+        showlegend=True,  # show the legend
+        legend_title='Caption Category',  # set the title of the legend
+        # template='plotly_white',
+        legend_font=dict(family='Arial', size=10),  # set the font family and size of the legend
+
+    )
+    fig.update_traces(hovertemplate="<b>Category:</b> %{customdata[0]}<br>" +
+                                    "<b>Caption:</b> %{customdata[1]}<br>")
+
+    return fig
+
+
+def plot_image_clusters(df):
+    marker_chart = alt.Chart(df.rename(columns={'url': 'image'})).mark_circle().encode(
+                    x='x',
+                    y='y',
+                    size='size',
+                    color='artist',
+                    tooltip=['image', 'artist']).properties(
+                    width=800,
+                    height=600,
+                ).configure_legend(
+        disable=True
+    )
+    return marker_chart
 def tech_summary_side_bar(config_key):
     config = load_yaml("config.yaml")
     expander_info = config['MORE_INFO'][config_key]
