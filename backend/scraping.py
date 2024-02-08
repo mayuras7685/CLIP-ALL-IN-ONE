3e1451d8019b75734a56ec75ea2a82f8bcc790aa
mayuras7685 <mayurasodara@gmail.com>
2024-02-08 12:44:15 +0530

    bbc headline scrap

--- backend/scraping.py
+++ backend/scraping.py
@@ -32,3 +32,24 @@ def scrape_unsplash_urls(prefix, categories):
         category_soup = get_soup(response)
         images_scraped.extend(get_unsplash_urls_from_soup(category_soup))
     return images_scraped
+
+
+def get_bbc_headlines_from_soup(soup):
+    bbc_headlines = []
+    # Find all the headlines using the "h3" tag and "gs-c-promo-heading" class
+    headlines= soup.find_all("p")
+    # Loop through each headline and print the text
+    for headline in headlines:
+        try:
+            bbc_headlines.append(headline.get_text())
+        except:
+            pass
+    return [i for i in bbc_headlines if len(i.split()) >=7]
+
+def scrape_bbc_headlines(prefix, categories: list):
+    headlines = []
+    for category in categories:
+        response = get_response(prefix, category)
+        category_soup = get_soup(response)
+        headlines.extend(get_bbc_headlines_from_soup(category_soup))
+    return headlines
