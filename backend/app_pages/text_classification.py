7a0699e4878f571720bdd3b8cc08318845c5fc73
mayuras7685 <mayurasodara@gmail.com>
2024-02-08 12:34:30 +0530

    text classification pipeline

---
+++ backend/app_pages/text_classification.py
@@ -0,0 +1,13 @@
+from backend.pipeline_text_classification import get_bbc_headlines, text_classification_loop
+import streamlit as st
+from backend.show import tech_summary_side_bar
+
+
+def write(tokeniser, model):
+  tech_summary_side_bar('Zero_shot_classification_text')
+  st.title('Zero Shot Text Classification - BBC Headlines')
+  st.markdown('#')
+  st.markdown('#')
+  bbc_headlines = get_bbc_headlines()
+  text_classification_loop(bbc_headlines, tokeniser, model)
+  return
