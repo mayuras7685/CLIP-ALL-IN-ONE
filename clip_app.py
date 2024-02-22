83e3f58b7d4a1887928a5a3b24540f30ddba90fb
mayuras7685 <mayurasodara@gmail.com>
2024-02-22 17:54:27 +0530

    Image Clustering added

--- clip_app.py
+++ clip_app.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import streamlit as st
-from backend.app_pages import image_classification, text_classification
+from backend.app_pages import image_classification, text_classification, image_clustering, semantic_searh
 from backend.clip_functions import download_clip_model
 from backend.load import load_yaml
-
+from backend.app_pages import data
 
 
 st.set_page_config(layout="wide")
@@ -18,9 +18,12 @@ clip_model = config['CLIP_MODEL'][0]
 model, processor, tokeniser = download_clip_model(clip_model) # load the model, processor and tokeniser - this is cached
 
 # Define menu items for each sub-app
+# Define menu items for each sub-app
 navigation_buttons = {"Zero Shot Image Classification": image_classification,
                       "Zero Shot Text Classification": text_classification,
-                      }
+                      "Image Clustering": image_clustering,
+                      "Semantic Search": semantic_searh,
+}
 
 st.sidebar.title('CLIP Demo')
 selection = st.sidebar.radio("Go to", list(navigation_buttons.keys()))
@@ -30,3 +33,9 @@ if selection == 'Zero Shot Image Classification':
 
 elif selection == 'Zero Shot Text Classification':
     page.write(tokeniser, model)
+
+elif selection == 'Image Clustering':
+    page.write(df_cached_embeddings_rock, processor, model)
+
+elif selection == 'Semantic Search':
+    page.write(df_cached_embeddings_rock, model, tokeniser)
